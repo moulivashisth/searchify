1226ebd37d674dd403e54289a451e05b3cde7365
Chandramouli Vashisth <chandramoulivashisth@gmail.com>
2017-09-19 21:54:15 +0530

    added query parameter

--- service.js
+++ service.js
@@ -1,16 +1,13 @@
 app.service("MVCService", function ($http) {
-  var url = "https://affiliate-api.flipkart.net/affiliate/1.0/search.json"
+  var url = "https://affiliate-api.flipkart.net/affiliate/1.0/search.json?query=sony"
    this.getData = function ($scope) {     
        $http.jsonp(url, {
            callback: 'JSON_CALLBACK',
            method: "GET",
-           params : {
-            query : "sony"
-           },
-    headers: {
-      "Access-Control-Allow-Origin":"*",
-    "fk-affiliate-id": "chandramo9",
-    "fk-affiliate-token": "f8f01e9213be419fb1ed7c76328a923d"
+           headers: {
+              "Access-Control-Allow-Origin":"*",
+              "fk-affiliate-id": "chandramo9",
+              "fk-affiliate-token": "f8f01e9213be419fb1ed7c76328a923d"
     
   }
           // params: { user_id: user.id }
